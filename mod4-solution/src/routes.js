c5ab24b4a0df5b0d2b4d4b1606b64506ecdc9c7f
Linda Houston <lindajean54@cableone.net>
2021-04-02 22:34:46 -0500

    test

--- mod4-solution/src/routes.js
+++ mod4-solution/src/routes.js
@@ -13,13 +13,13 @@ angular.module('MenuApp')
    $stateProvider
    // home page
    .state('home', {
-     url: '/',
-     templateUrl: 'menulist/templates/home.template.html'
+     url: '/menulist/templates/home',
+     templateUrl: 'src/menulist/templates/home.html'
    })
 
    .state('categories', {
-       url: '/categories',
-       templateUrl: 'menulist/templates/categories.template.html',
+       url: '/menulist/templates/categories',
+       templateUrl: 'src/menulist/templates/categories.html',
        controller: 'categoriesController as categoriesCtrl',
        resolve: {
          categories: ['MenuDataService', function (MenuDataService) {
@@ -29,8 +29,8 @@ angular.module('MenuApp')
      })
 
    .state('items', {
-       url: '/categories/{categoryShortName}',
-       templateUrl: 'menulist/templates/items.template.html',
+       url: '/menulist/templates/categories/{categoryShortName}',
+       templateUrl: 'src/menulist/templates/items.html',
        controller: 'itemsController as itemsCtrl',
        params: {
          categoryShortName: null,
